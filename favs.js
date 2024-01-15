c5ac4102b4442808fc6f8dcd95b48dbd70c78a16
Pavlo <winterspray@gmail.com>
2024-01-15 14:49:24 -0500

    dd

--- favs.js
+++ favs.js
@@ -3,22 +3,27 @@ import { arr } from './voting.js';
 const content = document.querySelector('.block__content');
 
 export const renderFavs = () => {
-	const uniqueArray = arr.filter(
-		(value, index, self) => self.indexOf(value) === index
-	);
-	console.log(uniqueArray);
+	if (arr.length !== 0) {
+		const uniqueArray = arr.filter(
+			(value, index, self) => self.indexOf(value) === index
+		);
+		console.log(uniqueArray);
+		localStorage.setItem('favorite', JSON.stringify(uniqueArray));
+	}
 
-	const favs = `<h1>Favs</h1>`;
+	const favs = `<h1>Nothing in favs</h1>`;
 	content.innerHTML = favs;
+	const storedFavs = JSON.parse(localStorage.getItem('favorite'));
 
-	const box = document.createElement('div');
-	box.classList.add('box');
+	if (storedFavs && storedFavs.length !== 0) {
+		const box = document.createElement('div');
+		box.classList.add('box');
 
-	const boxContent = uniqueArray.map(item => {
-		return `<img class='box__img' src='${item}'/>`;
-	});
-
-	box.innerHTML = boxContent.join('');
-
-	content.appendChild(box);
+		const boxContent = storedFavs.map(
+			item => `<img class='box__img' src='${item}'/>`
+		);
+		box.innerHTML = boxContent.join('');
+		content.innerHTML = '';
+		content.appendChild(box);
+	}
 };
